8f74e5ed1cc3edb584389196a8900227448a61c5
im-Lily <67992684+im-Lily@users.noreply.github.com>
2021-05-16 16:07:14 +0900

    update detect.py
    result save json file

--- detect.py
+++ detect.py
@@ -105,11 +105,6 @@ def detect(opt):
                 # Write results
                 print('-->Write results')
                 for *xyxy, conf, cls in reversed(det):
-                    # if save_txt:  # Write to file
-                    #     label = f'{names[int(cls)]} {conf:.2f}' # detected label name
-                    #     with open(txt_path + '.txt', 'a') as f:
-                    #         f.write(label +  '\n')
-                    #     print(label)
                     if save_img or view_img:  # Add bbox to image
                         k = f'{names[int(cls)]}'
                         v = float(f'{conf:.2f}')
@@ -121,20 +116,10 @@ def detect(opt):
                     if value == max_label:
                         result_dict = {key:value}
                         print(result_dict)
-                        
 
-                # print(max_label)
-                # print(type(max_label))
-                        #labels_dict = {string:i for i,string in enumerate(labels)}
-                    # labels.append(label)
-                # print(labels)
+                with open("result_score.json", "w") as json_file:
+                    json.dump(result_dict, json_file)         
                     
-                    
-
-                #     with open("result_score.json", "w") as json_file:
-                #         json.dump(labels, json_file)
-                # print(labels_dict)
-
 
             # Print time (inference + NMS)
             print(f'{s}Done. ({t2 - t1:.3f}s)')
