f560eadbc2e300025ad65b44c3f69b9df9d3747a
Sim Youngseok <46801877+Supreme-YS@users.noreply.github.com>
2021-05-29 23:34:57 +0900

    Update detect.py

--- base_code/detect.py
+++ base_code/detect.py
@@ -5,7 +5,6 @@ import cv2
 import torch
 from numpy import random
 import json
-
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
